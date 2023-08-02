be49540c6920f16abde1e2a6fe6bf8ef3c90a687
Alexander Valladares <edwin.valladares23@itca.edu.sv>
2023-08-02 11:15:21 -0600

    Codigo para mostrar .txt
    Muestra en consola documentos de texto planos como .txt en una ruta establecida previamente.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string documento = "/Users/edwinvalladares/Desktop/doc.txt";
+        //documento en mi escritorio. 
+
+        try
+        {
+            string contenido = File.ReadAllText(documento);
+            Console.WriteLine("Contenido del archivo:");
+            Console.WriteLine(contenido);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Error al leer el archivo: " + ex.Message);
+        }
+    }
+}
+
